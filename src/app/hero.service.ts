16436b76bb6fd6ed9fb1c474bf748f64623277aa
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-12-20 00:39:46 +0100

    Add option to update a hero
    Change-Id: Ibdb6c74c0d6b3283797c29496a09711f829c1a3a

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,11 +5,16 @@ import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
 import { catchError, map, tap } from "rxjs/operators";
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
+
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
   private heroesUrl = "api/heroes"; // URL to web api
+;
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
@@ -24,6 +29,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
